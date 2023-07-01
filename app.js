e0ad653c2a13f977a3134f5b1beef0ed3070e3a2
Loren Seballes <seballesloren@gmail.com>
2023-07-01 20:28:27 -0300

    Detalles de css y terminaciones finales

--- app.js
+++ app.js
@@ -1,206 +1,220 @@
-  const list = document.querySelector(".listProductos");
+const list = document.querySelector(".listProductos");
 
 
-  const llamarProd= async()=>{
-  const resp = await fetch("./data.json");
-  const data = await resp.json();
-  
-  data.forEach((prod)=>{
-  const li = document.createElement('li');
-    li.innerHTML=
-      ` <div id="contProductos" class="card-group">
-        <div class="card">
-        <img src="${prod.imagen}" class="card-img-top" alt="producto" />
-          <div class="card-body">
-            <h5 class="card-title">${prod.nombre}</h5>
-            <h3 class="precio">U$S ${prod.precio}</h3>
-            <button id="agregar" class="agregar-carrito btn btn-dark" data-product-id="${prod.id}">Agregar al carrito</button>
-          </div>
+const llamarProd= async()=>{
+const resp = await fetch("./data.json");
+const data = await resp.json();
+
+data.forEach((prod)=>{
+const li = document.createElement('li');
+  li.innerHTML=
+    ` <div id="contProductos" class="card-group">
+      <div class="card">
+      <img src="${prod.imagen}" class="card-img-top" alt="producto" />
+        <div class="card-body">
+          <h5 class="card-title">${prod.nombre}</h5>
+          <h3 class="precio">U$S ${prod.precio}</h3>
+          <button id="agregar" class="agregar-carrito btn btn-dark" data-product-id="${prod.id}">Agregar al carrito</button>
         </div>
       </div>
-      `;
+    </div>
+    `;
 
-          list.append(li)
-  })
+        list.append(li)
+})
 
-  let carrito = []
+let carrito = []
 
-  const botonesAgregar = document.querySelectorAll(".agregar-carrito");
+const botonesAgregar = document.querySelectorAll(".agregar-carrito");
 
-  botonesAgregar.forEach((boton) => {
-  boton.addEventListener("click", agregarAlCarrito);
+botonesAgregar.forEach((boton) => {
+boton.addEventListener("click", agregarAlCarrito);
 
 
 });
 
-  function agregarAlCarrito(event) {
-  const boton = event.target;
-  const producto = obtenerProductoDesdeBoton(boton);
-  carrito.push(producto);
-  mostrarCarrito();
+function agregarAlCarrito(event) {
+const boton = event.target;
+const producto = obtenerProductoDesdeBoton(boton);
+carrito.push(producto);
+mostrarCarrito();
+
+// Agregar mensaje al DOM
+const mensaje = document.createElement('p');
+mensaje.textContent = 'Se agregó al carrito';
+mensaje.classList.add('mensaje-agregado'); 
+boton.parentNode.appendChild(mensaje);
+
+// Borrar el mensaje después de un tiempo determinado 
+setTimeout(() => {
+  mensaje.remove();
+}, 2000); 
+
 }
-  function obtenerProductoDesdeBoton(boton) {
-  const productoId = boton.dataset.productId;
-  const producto = data.find((prod) => prod.id === parseInt(productoId));
-  return producto;
+function obtenerProductoDesdeBoton(boton) {
+const productoId = boton.dataset.productId;
+const producto = data.find((prod) => prod.id === parseInt(productoId));
+return producto;
 }
-  function mostrarCarrito() {
-  console.log("Productos en el carrito:");
-  console.log(carrito);
- }
+function mostrarCarrito() {
+console.log("Productos en el carrito:");
+console.log(carrito);
+}
 
-  const verCarrito = document.querySelector("#verCarrito");
-  const modalCarrito = document.querySelector("#modalCarrito");
-  const contenido = document.querySelector("#contenido");
+const verCarrito = document.querySelector("#verCarrito");
+const modalCarrito = document.querySelector("#modalCarrito");
+const contenido = document.querySelector("#contenido");
 
-  verCarrito.addEventListener("click", mostrarCarritoModal);
+verCarrito.addEventListener("click", mostrarCarritoModal);
 
-  function mostrarCarritoModal () {
-    contenido.innerHTML = " ";
+function mostrarCarritoModal () {
+  contenido.innerHTML = " ";
 
-  if (carrito.length === 0) {
-    contenido.innerHTML = "<p>El carrito está vacío.</p>";
-  } else {
+if (carrito.length === 0) {
+  contenido.innerHTML = "<p>El carrito está vacío.</p>";
+} else {
 
-    const productosHTML = carrito.map((producto, index) => {
-      return `
-        <div>
-          <h3>${producto.nombre}</h3>
-          <p>Precio: U$S${producto.precio}</p>
-          <p>Cantidad: ${producto.cantidad}</p>
-          <button class="eliminar-producto" data-product-index="${index}">
-              <i class="fa fa-trash"></i> Eliminar
-            </button>
-        </div>
-      `;
-    })
-    .join("");
+  const productosHTML = carrito.map((producto, index) => {
+    return `
+      <div>
+      <img src="${producto.imagen}" alt="${producto.nombre}" class="producto-imagen" />
+        <h3>${producto.nombre}</h3>
+        <p>Precio: U$S${producto.precio}</p>
+        <p>Cantidad: ${producto.cantidad}</p>
+        <button class="eliminar-producto" data-product-index="${index}">
+            <i class="fa fa-trash"></i> Eliminar
+          </button>
+      </div>
+    `;
+  })
+  .join("");
 
-    // Agregar los productos al contenedor del carrito
-    contenido.innerHTML = productosHTML;
+  // Agregar los productos al contenedor del carrito
+  contenido.innerHTML = productosHTML;
 
-    const total = carrito.reduce((accumulator, producto) => {
-        return accumulator + producto.precio * producto.cantidad;
-    }, 0);
+  const total = carrito.reduce((accumulator, producto) => {
+      return accumulator + producto.precio * producto.cantidad;
+  }, 0);
 
-    const totalComp = document.querySelector("#totalComp");
-    totalComp.textContent = total;
-    // Agregar eventos de clic a los botones de eliminar
-    const botonesEliminar = document.querySelectorAll(".eliminar-producto");
-    botonesEliminar.forEach((boton) => {
-      boton.addEventListener("click", eliminarProducto);
-    });
-  }
+  const totalComp = document.querySelector("#totalComp");
+  totalComp.textContent = total;
+  
+  // Agregar eventos de clic a los botones de eliminar
+  const botonesEliminar = document.querySelectorAll(".eliminar-producto");
+  botonesEliminar.forEach((boton) => {
+    boton.addEventListener("click", eliminarProducto);
+  });
+}
 
-  //Se pasa el array a JSON
-  const carro = JSON.stringify(carrito)
-  localStorage.setItem("prod", carro)
+//Se pasa el array a JSON
+const carro = JSON.stringify(carrito)
+localStorage.setItem("prod", carro)
 
-  //Se obtiene información del storage
+//Se obtiene información del storage
 
-  const carritoGuardado = localStorage.getItem('carro');
-     if (carritoGuardado) {
-   carro = JSON.parse(carritoGuardado);
+const carritoGuardado = localStorage.getItem('carro');
+   if (carritoGuardado) {
+ carro = JSON.parse(carritoGuardado);
 } 
 
-  // Mostrar el modal del carrito
-    modalCarrito.style.display = "block";
- }
- function eliminarProducto(event) {
-  const boton = event.target;
-  const productoIndex = boton.dataset.productIndex;
-  carrito.splice(productoIndex, 1);
-  mostrarCarritoModal();
+// Mostrar el modal del carrito
+  modalCarrito.style.display = "block";
 }
- //vaciar carrito 
+function eliminarProducto(event) {
+const boton = event.target;
+const productoIndex = boton.dataset.productIndex;
+carrito.splice(productoIndex, 1);
+mostrarCarritoModal();
+}
+//vaciar carrito 
 
-  const botonVaciarCarrito = document.querySelector("#eliminar");
-  const totalCompElemento = document.querySelector("#totalComp");
+const botonVaciarCarrito = document.querySelector("#eliminar");
+const totalCompElemento = document.querySelector("#totalComp");
 
-  botonVaciarCarrito.addEventListener("click", vaciarCarrito);
+botonVaciarCarrito.addEventListener("click", vaciarCarrito);
 
-  function vaciarCarrito() {
-  carrito = []; 
-  mostrarCarrito(); 
-  totalCompElemento.textContent = "0";
+function vaciarCarrito() {
+carrito = []; 
+mostrarCarrito(); 
+totalCompElemento.textContent = "0";
 }
 
 }
- llamarProd()
+llamarProd()
 
 
-        //Saludo
+ //Saludo
 
-   const Saludo = document.querySelector("#eliminar")
+ const Saludo = document.querySelector("#eliminar")
 
-  Saludo.addEventListener("click", ()=> {
-    Swal.fire('Regresa cuando quieras, aquí te esperamos')
+Saludo.addEventListener("click", ()=> {
+  Swal.fire('Regresa cuando quieras, aquí te esperamos')
 
-    }) 
+  }) 
 
-  //finalizar
+//finalizar
 
-  const fin = document.querySelector("#finalizar")
+const fin = document.querySelector("#finalizar")
 
-  fin.addEventListener("click", ()=> {
-    Swal.fire(
-        ' ',
-        'Tu compra se ha realizado con éxito :)',
-        'success'
-      )
+fin.addEventListener("click", ()=> {
+  Swal.fire(
+      ' ',
+      'Tu compra se ha realizado con éxito :)',
+      'success'
+    )
 
-    }) 
+  }) 
 
 
 // Ingresar mail
 
-  const btn = document.querySelector("#button-addon2")
+const btn = document.querySelector("#button-addon2")
 
-  btn.addEventListener("click", ()=> {
-    Swal.fire(
-        'Gracias por dejarnos tu mail',
-        'Nos pondremos en contacto contigo :)',
-        'success'
-      )
+btn.addEventListener("click", ()=> {
+  Swal.fire(
+      'Gracias por dejarnos tu mail',
+      'Nos pondremos en contacto contigo :)',
+      'success'
+    )
 
-    }) 
+  }) 
 
-    //Inicio de sesión
+  //Inicio de sesión
 
-    var modal = document.querySelector("#modal");
-    var openModalBtn = document.querySelector("#open-modal");
+  var modal = document.querySelector("#modal");
+  var openModalBtn = document.querySelector("#open-modal");
 
-    openModalBtn.addEventListener("click", function() {
-    modal.style.display = "block"; // Mostrar el modal al hacer clic en el botón
-  });
+  openModalBtn.addEventListener("click", function() {
+  modal.style.display = "block"; // Mostrar el modal al hacer clic en el botón
+});
 
-    document.addEventListener("click", function(event) {
-    if (event.target === modal) {
-    modal.style.display = "none"; // Ocultar el modal al hacer clic fuera de él
+  document.addEventListener("click", function(event) {
+  if (event.target === modal) {
+  modal.style.display = "none"; // Ocultar el modal al hacer clic fuera de él
+}
+});
+
+
+  document.querySelector("#login-form").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  // Obtener los valores de los campos de entrada
+  var username = document.querySelector("#username").value;
+  var password = document.querySelector("#password").value;
+
+  // Verificar datos 
+  if (username === "Omar" && password === "1234") {
+    // Inicio de sesión exitoso
+    mostrarNombreUsuario(username);
+    modal.style.display = "none"; 
+  } else {
+    // Usuario/contraseña incorrectas
+    alert("Nombre de usuario o contraseña incorrectos");
   }
-  });
+});
 
-
-    document.querySelector("#login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-  
-    // Obtener los valores de los campos de entrada
-    var username = document.querySelector("#username").value;
-    var password = document.querySelector("#password").value;
-  
-    // Verificar las credenciales 
-    if (username === "Omar" && password === "1234") {
-      // Inicio de sesión exitoso
-      mostrarNombreUsuario(username);
-      modal.style.display = "none"; 
-    } else {
-      // Usuario/contraseña incorrectas
-      alert("Nombre de usuario o contraseña incorrectos");
-    }
-  });
-  
-  function mostrarNombreUsuario(username) {
-    var mensaje = "Bienvenido, " + username + "!";
-    document.querySelector("#nombre-usuario").textContent = mensaje;
-  }
+function mostrarNombreUsuario(username) {
+  var mensaje = "Bienvenido, " + username + "!";
+  document.querySelector("#nombre-usuario").textContent = mensaje;
+}
   
